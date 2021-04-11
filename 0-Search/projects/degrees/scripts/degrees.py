f061aaf75f894fae510049dbea4fe117d6afba8c
Artur Amaral <artur_amaral@poli.ufrj.br>
2021-04-10 22:15:21 -0300

    Initial commit. Personal comments added to the code downloaded from cs50's site.

---
+++ 0-Search/projects/degrees/scripts/degrees.py
@@ -0,0 +1,146 @@
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"../data/{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"../data/{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"../data/{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    # O diretório acessado para obter os dados depende do argumento dado na CLI
+    # No caso de omissão, ele acessa diretamento o diretório large/. Caso queiramos
+    # Acessar os dados de small/ devemos especificar através do argv.
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+
+    # Entrada do ator inicial (Estado inicial).
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    # Entrada do ator final (Estado final).
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    # Função que vou implementar com o algoritmo BFS.
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+
+    # TODO
+    raise NotImplementedError
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0-Search/projects/degrees/scripts/util.py
@@ -0,0 +1,68 @@
+#------------------------------------------------------------------
+# O conceito de Nó é implementado como uma classe. Um nó é uma estrutura de dados que,
+# nesse contexto, codifica as informações de estado atual, nó pai, ação que o trouxe do
+# nó pai e o modelo de transição deste nó para todos seus possíveis filhos.
+#
+# A classe contém o método de inicialização __init__ (acredito que seja o construtor)
+# que define seus atributos fundamentais e atribui a eles os valores recebidos nos argumentos
+# durante a declaração do objeto.
+#------------------------------------------------------------------
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+#------------------------------------------------------------------
+# O conceito de fronteira também é implementado como uma classe.
+# Fronteira é o conjunto de nós a serem analisados em dados instante.
+# Ou seja, são nós que já foram acessados, mas ainda não foram submetidos
+# a teste para validá-los como solução.
+#
+# Há duas implementações de fronteira: Uma como pilha, outra como fila.
+# Cada uma tem sua vantajem. No caso de pilha, o algoritmo realiza primeiro
+# uma busca até o fim de um ramo. Já para fila, a busca é feita igualmente
+# alternadamente entre os ramos do grafo.
+#
+# No problema Degrees, como pretendo implementar uma Breadth First Search,
+# utilizarei a implementação da fronteira como fila.
+#------------------------------------------------------------------
+
+class StackFrontier():
+    # O atributo frontier é uma lista comum do Python.
+    def __init__(self):
+        self.frontier = []
+
+    # Este método adiciona ao fim da fronteira (Uma lista) o nó especificado no argumento.
+    # (Lembrando que o nó é um objeto.)
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def isEmpty(self):
+        return len(self.frontier) == 0
+
+
+    # Retira o último nó da fronteira.
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    # Retira o primeiro nó da fronteira
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
